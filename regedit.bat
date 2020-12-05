f7deb645b4515968bb97d3d984d93735f00ff317
HtinLinnZaw <htinlinnzaw.dev@gmail.com>
2020-12-05 22:00:14 +0630

    1. add: registry control program

---
+++ regedit.bat
@@ -0,0 +1,19 @@
+@echo off
+prompt $p$g
+title Control Registry Editor
+:control
+cls
+echo Control Registry Editor
+echo 1. Disable Registry Editor
+echo 2. Enable Registry Editor
+set/p val = Your choicel
+if "%val%" == "1" goto -1
+if "%val%" == "2" goto -2
+:-1
+reg add hkcu\software\microsoft\windows\currentversion\policies\explorer /v disableregistrytools /t reg_dword /d 1 /f
+pause
+goto :control
+:-2
+reg delete hkcu\software\microsoft\windows\currentversion\policies\explorer /v disableregistrytools /f
+pause
+goto :control
